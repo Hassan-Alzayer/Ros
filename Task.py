00672408b2d6db26efde730797d83c913558f874
HassanCode985 <108063654+HassanCode985@users.noreply.github.com>
2022-07-22 15:21:47 +0300

    Update Task.py

--- Task.py
+++ Task.py
@@ -12,32 +12,32 @@
 #--- Installation: 
 sudo apt update
 
-#4 Desktop Install: Everything in Desktop plus 2D/3D simulators and 2D/3D perception packages 
+#--- Desktop Install Everything in Desktop plus 2D/3D simulators and 2D/3D perception packages 
 sudo apt install ros-noetic-desktop-full
 
-#5 Desktop Install: Everything in ROS-Base plus tools like rqt and rviz
+#--- Desktop Install: Everything in ROS-Base plus tools like rqt and rviz
  sudo apt install ros-noetic-desktop
 
-#6 ROS-Base: (Bare Bones) ROS packaging, build, and communication libraries. No GUI tools
+#--- ROS-Base ROS packaging, build, and communication libraries. No GUI tools
  sudo apt install ros-noetic-ros-base
 
-#7There are even more packages available in ROS. You can always install a specific package directly. 
+#--- There are even more packages available in ROS. You can always install a specific package directly:
 sudo apt install ros-noetic-PACKAGE
 
-#- e.g.
+#--- e.g: 
 sudo apt install ros-noetic-slam-gmapping
 
-#8 Environment setup
+#--- Environment setup
  source /opt/ros/noetic/setup.bash
 
-#9 It can be convenient to automatically source this script every time a new shell is launched. These commands will do that for you.
+#--- It can be convenient to automatically source this script every time a new shell is launched : 
 
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc source ~/.bashrc
 
-#10 Dependencies for building packages: 
+#--- Dependencies for building packages 
 sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 
-#11 Initialize rosdep: 
+#--- Initialize rosdep: 
 sudo apt install python3-rosdep
 
 
